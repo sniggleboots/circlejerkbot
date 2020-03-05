6b871b56c611279c6c2b91c39184ceb38fc40373
tharris99 <traylee1513@yahoo.com>
2020-03-05 05:14:54 -0800

    Simplify Postgres connection

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const { Client } = require('pg');
-
-const client = new Client({
+const { Pool } = require('pg');
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 
-client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 
 var thisAlgorithmBecomingSkynetCost = 99999999;
