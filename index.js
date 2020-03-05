c4b566c40191c30da651e70c0b130e97c5e20ab4
tharris99 <traylee1513@yahoo.com>
2020-03-05 04:53:45 -0800

    Initial additions of Postgres

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-//const botconfig = require("./botconfig.json");
-//const tokenfile = require("./token.json");
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 
 var thisAlgorithmBecomingSkynetCost = 99999999;
 
