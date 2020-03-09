56e81b20e1145750e79f274f3872e2357bcdfe5f
tharris99 <traylee1513@yahoo.com>
2020-03-09 06:14:03 -0700

    Fix to sentence being lost on dyno reset

--- index.js
+++ index.js
@@ -1,95 +1,112 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+const Discord = require("discord.js")
+const bot = new Discord.Client({disableEveryone: true})
+const gweedoId = "121044023765893121"
+const sniggleId = "220289568165855242"
+const botId = "496280185193365505"
+const channelId = "498890427257061397"
+global.channel
+var messages
+var CooldownInS = 5
+var lastPoster = "-1"
+var secondLastPoster = "0"
+var collectionString = ""
 
-
-
-
-var thisAlgorithmBecomingSkynetCost = 99999999;
-
-const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
-var CooldownInS = 5;
-var lastPoster = "-1";
-var secondLastPoster = "0";
-var collectionString = "";
-var gerbilId = "129732922096746497"
-var botId = "544698009254887445"
 
 bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity("#circlejerk");
-});
+    console.log(`${bot.user.username} is online!`)
+    bot.user.setActivity("#circlejerk")
+    channel = bot.channels.get(channelId)
+    channel.send("Ready")
+})
+
 
 bot.on("message", async message => {
-    //sniggleboots and gweedo can DM the bot to stop it or change the personal cooldown
-
-    if(message.channel.type == "dm" && (message.author.id == "220289568165855242" || message.author.id == "121044023765893121"))
+   
+    //On DM event. Checks for Gweedo or Sniggle as sender
+    if(message.channel.type == "dm" && (message.author.id == sniggleId || message.author.id == gweedoId))
     {
         if(message.content === "stop" || message.content === "quit")
         {
-            console.log(`bot halted by ${message.author.username}.`);
+            console.log(`bot halted by ${message.author.username}.`)
             message.channel.send("Shutting down.")
-            bot.destroy();
+            bot.destroy()
         }
     }
 
     /*
-    first, check if this only happens in the circlejerk channel, to avoid shenanigans if the bot ever gains permissions anywhere else - which it shouldn't
-    then, check whether the posted message has more than one word, or if it's a double post
-    if yes: delete it
-    if no: disallow posting in the channel for everyone, then set a timer that allows posting after CooldownInS seconds
+    1st Check if message in channel named circlejerk
+    2nd Check if message is double post or more than one word and delete, else post it
     */
 
     if(message.channel.name === "circlejerk")
     {
-        //split the message by spaces
-        var splitmessage = message.content.split(" ");
-        //if a second part exists, it means there was a space and the message should be deleted, unless it is a message by the bot.
-        if((splitmessage[1] || message.author.id == lastPoster) && message.author.id !== botId && message.author.id !== gerbilId)
+        if(message.content === "Ready" && message.author.id === botId)
         {
-            message.delete(0);
-            console.log(`deleted message by ${message.author.username}`+`#${message.author.discriminator}.`);
-        }else
-        {
-            //set post permission to false for message author, unless author is the bot
-	    /*
-            if(message.author.id !== "496280185193365505")
-            {
-                message.channel.overwritePermissions(message.author, {'SEND_MESSAGES':false},);
-            }
-	    */
+                messages = await message.channel.fetchMessages()
+                messages.array()
+                msgArray = messages._array
 
-            lastPoster = message.author.id;
-            console.log(`accepted message by ${message.author.username}`+`#${message.author.discriminator}.`);
-
-            //after COOLDOWN minutes, set post permission to true for message author, unless author is the bot. Doesn't really matter if it's the bot in this case, it's just to keep the log clean.
-	    /*
-            if(message.author.id !== "496280185193365505")
-            {
-                setTimeout(function(){ 
-                    message.channel.overwritePermissions(message.author, {'SEND_MESSAGES':true},); 
-                    console.log(`reset posting permissions for ${message.author.username}`+`#${message.author.discriminator}.`);
-                }, 1000 * CooldownInS);
-            }
-	    */
-
-            collectionString += " ";
-            collectionString += message.content;
-
-            if(message.content.slice(-1) == "?" || message.content.slice(-1) == "." || message.content.slice(-1) == "!"){
-                if(message.author.id !== botId){
-                    secondLastPoster = lastPoster;
-                    message.channel.send(collectionString);
+                var userMsgArray = []
+                var i = 1 //Increment should start at 1 since the first message in array should always be the bot.
+                
+                while(msgArray[i].author.id !== botId) //Could become an infinite loop if there are no bot messages in the past 50 messages.
+                {
+                    userMsgArray[i - 1] = msgArray[i] //Create an array of user messages until there was a bot message.
+                    i++
                 }
-                collectionString = "";
-                lastPoster = secondLastPoster;
+
+                //Rebuild the collectiong string from the end of the user message array
+                var j = userMsgArray.length - 1
+                while(j + 1 != 0)
+                {
+                        if (j != 0)
+                        {
+                            collectionString += userMsgArray[j] + " "
+                        }
+                        else
+                        {
+                            collectionString += userMsgArray[j]
+                        }
+                    j--
+                }
+        }
+        else
+        {
+            //split the message by spaces
+            var splitmessage = message.content.split(" ")
+            //if a second part exists, it means there was a space and the message should be deleted, unless it is a message by the bot.
+            if((splitmessage[1] || message.author.id == lastPoster) && message.author.id !== botId)
+            {
+                message.delete(0)
+                console.log(`deleted message by ${message.author.username}`+`#${message.author.discriminator}.`)
+            }
+            else
+            {
+                var slicedString = message.content.slice(-1)
+                lastPoster = message.author.id
+                console.log(`accepted message by ${message.author.username}`+`#${message.author.discriminator}.`)
+                switch (slicedString)
+                {
+                    case "!":
+                    case ".":
+                    case "?":
+                        collectionString += message.content
+
+                        if(message.author.id !== botId){
+                            secondLastPoster = lastPoster
+                            message.channel.send(collectionString)
+                        }
+
+                        collectionString = ""
+                        lastPoster = secondLastPoster
+                        break
+                    default:
+                        collectionString += " "
+                        collectionString += message.content       
+                }
             }
         }
     }
-});
 
-
-bot.login(process.env.token);
+})
+bot.login(process.env.token)
